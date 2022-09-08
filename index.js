e295366a08820a091a7ea75839f9697cd05ad808
patoary <zahidsagar93@gmail.com>
2022-09-09 03:11:07 +0900

    put user/:email api to add a new user

--- index.js
+++ index.js
@@ -20,45 +20,58 @@ async function run() {
         const productCollection = client.db('sk_saw').collection('products');
         const reviewCollection = client.db('sk_saw').collection('reviews');
         const orderCollection = client.db('sk_saw').collection('orders');
+        const userCollection = client.db('sk_saw').collection('users');
 
+        // to add a new user
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        });
 
         app.get('/product', async (req, res) => {
             const query = {};
             const product = await productCollection.find(query).toArray();
             res.send(product);
         });
-        
-        app.get('/product/:id', async(req, res) => {
+
+        app.get('/product/:id', async (req, res) => {
             const id = req.params;
-            const query ={ _id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await productCollection.findOne(query);
             res.send(result);
         });
 
-        app.get('/review', async(req, res) => {
+        app.get('/review', async (req, res) => {
             const review = await reviewCollection.find().toArray();
             res.send(review);
         });
-        app.post('/review', async(req, res) =>{
+        app.post('/review', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send({ success: true, result });
         });
 
-        app.post('/order', async(req, res) =>{
+        app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
-            res.send({ success: true, result});
+            res.send({ success: true, result });
         });
 
-        app.get('/order', async(req, res)=>{
+        app.get('/order', async (req, res) => {
             const result = await orderCollection.find().toArray();
             res.send(result);
         });
 
-        app.delete('/order/:id',async(req, res) =>{
+        app.delete('/order/:id', async (req, res) => {
             const id = req.params;
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await orderCollection.deleteOne(query);
             res.send(result);
         });
