2d25b248b8227f2353e3d7e38e68876609235e44
patoary <zahidsagar93@gmail.com>
2022-09-08 22:27:36 +0900

    order delete api to delete an order

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/order/:id',async(req, res) =>{
+            const id = req.params;
+            const query = {_id:ObjectId(id)}
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally {
 
